727d19f9e0426831d4fda169695f26586466ce76
hrfmmymt <hrfmmymt@gmail.com>
2018-04-02 19:06:45 +0900

    modified.

--- src/components/home/skills.js
+++ src/components/home/skills.js
@@ -1,66 +1,25 @@
 import { h, Component } from 'preact'
+import styled from 'styled-components'
 import styles from './skills.css'
-import { ResponsiveContainer, Treemap } from 'Recharts'
 
-const COLORS = ['#8889DD', '#9597E4', '#8DC77B', '#A5D297', '#E2CF45', '#F8C12D', '#8889DD', '#9597E4', '#8DC77B', '#A5D297', '#E2CF45', '#F8C12D']
+const MapWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1.5fr 1fr 1fr 3fr;
+  grid-template-rows: 180px 100px 80px 120px auto;
+  max-width: 100%;
+  width: 100%;
+`
 
-class CustomizedContent extends Component {
-  render() {
-    const { root, depth, x, y, width, height, index, payload, colors, rank, label } = this.props
+const MapItem = styled.div`
+  display: block;
+`
 
+class SkillMap extends Component {
+  render({ label, value }) {
+    const classes = 'value-' + value
     return (
-      <g>
-        <rect
-          x={x}
-          y={y}
-          width={width}
-          height={height}
-          style={{
-            fill: depth < 2 ? colors[Math.floor(index / root.children.length * 6)] : 'none',
-            stroke: '#fff',
-            strokeWidth: 2 / (depth + 1e-10),
-            strokeOpacity: 1 / (depth + 1e-10),
-          }}
-        />
-        {
-          depth === 1 ?
-          <text
-            x={x + width / 2}
-            y={y + height / 2 + 7}
-            textAnchor="middle"
-            fill="#fff"
-          >
-            { label }
-          </text>
-          : null
-        }
-      </g>
-    );
-  }
-}
-
-class SkillChart extends Component {
-  handleClick(e) {
-    console.log(e);
-  }
-
-  render(list) {
-    const data = list.data
-
-    return (
-      <ResponsiveContainer width="100%" aspect={2}>
-        <Treemap
-          className={styles.chart}
-          width={480}
-          height={400}
-          data={data}
-          dataKey="value"
-          stroke="#fff"
-          content={<CustomizedContent colors={COLORS} />}
-          onClick={this.handleClick}
-        />
-      </ResponsiveContainer>
-    );
+      <MapItem className={ classes }>{ label } - { value }</MapItem>
+    )
   }
 }
 
@@ -69,7 +28,9 @@ export default class Skills extends Component {
     return (
       <section className={styles.wrapper}>
         <h2>{props.title}</h2>
-        <SkillChart data={props.list} />
+        <MapWrapper>
+          { props.list && props.list.map(data => <SkillMap {...data} />) }
+        </MapWrapper>
       </section>
     )
   }
