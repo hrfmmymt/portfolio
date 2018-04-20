eb82e41b16b5df091eaba101e25cc842fe92ed7f
hrfmmymt <hrfmmymt@gmail.com>
2018-04-20 19:08:10 +0900

    modified.

--- src/routes/career/index.js
+++ src/routes/career/index.js
@@ -2,12 +2,39 @@ import { h, Component } from 'preact'
 import style from './style.css'
 import profile from '../../assets/profile.json'
 
+function imagesLoaded(parentNode) {
+  const imgElements = [...parentNode.querySelectorAll('img')]
+  for (let i = 0; i < imgElements.length; i += 1) {
+    const img = imgElements[i]
+    if (!img.complete) {
+      return false
+    }
+  }
+  return true
+}
+
 class CareerDetailList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      career: profile.career.list
+      career: profile.career.list,
+      loading: true
     }
+  }
+
+  handleImageChange = () => {
+    this.setState({
+      loading: !imagesLoaded(this.galleryElement)
+    })
+  }
+
+  goBack = e => {
+    e.preventDefault()
+    history.back()
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
   }
 
   render(item) {
@@ -16,7 +43,9 @@ class CareerDetailList extends Component {
     })
 
     const tagList = thisJob[0].topic_names.map((topicName, i) => (
-      <li key={i}>{topicName}</li>
+      <li key={i} className={style.tagItem}>
+        {topicName}
+      </li>
     ))
 
     return (
@@ -27,19 +56,25 @@ class CareerDetailList extends Component {
               # hello world
             </h1>
           </header>
-          <section>
+          <section ref={element => {
+            this.galleryElement = element
+          }}>
             <h2>{thisJob[0].job_title}</h2>
             <p>{thisJob[0].role}</p>
+            <img
+              src={`../../assets/careers/${thisJob[0].job_id}.png`}
+              onLoad={this.handleImageChange}
+              onError={this.handleImageChange}
+              className={style.image}
+              alt='meaningless image.'
+            />
+            <small>画像はイメージです。<br />the image is an image. you know what i mean?</small>
             <p>{thisJob[0].description}</p>
-            <ul>{tagList}</ul>
+            <ul className={style.tagList}>{tagList}</ul>
           </section>
-          <button
-            type="button"
-            className={style.backButton}
-            onClick={() => history.back()}
-          >
+          <a className={style.backButton} onClick={this.goBack} href="">
             go back
-          </button>
+          </a>
         </article>
       </main>
     )
