f0964b9c8420c19bbba54b2c6237e4a4223843ac
hrfmmymt <hrfmmymt@gmail.com>
2018-05-02 11:26:32 +0900

    critical redering path css

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,10 @@
+import criticalCssPlugin from 'preact-cli-plugin-critical-css'
+
 export default function(config, env, helpers) {
   const postcssLoader = helpers.getLoadersByName(config, 'postcss-loader')
   postcssLoader.forEach(({ loader }) => delete loader.options)
+
+  const options = {}
+
+  criticalCssPlugin(config, env, options)
 }
