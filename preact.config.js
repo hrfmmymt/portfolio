91ff8c02bb56282904737b253c7dbdfb79f8348f
hrfmmymt <hrfmmymt@gmail.com>
2018-06-27 16:05:43 +0900

    404

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,6 @@
 import criticalCssPlugin from 'preact-cli-plugin-critical-css'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
+const preactCliSwPrecachePlugin = require('preact-cli-sw-precache')
 
 export default function(config, env, helpers) {
   const postcssLoader = helpers.getLoadersByName(config, 'postcss-loader')
@@ -19,4 +20,9 @@ export default function(config, env, helpers) {
   config.plugins.push(
     new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }])
   )
+
+  const precacheConfig = {
+    staticFileGlobsIgnorePatterns: [/404\.html$/]
+  }
+  return preactCliSwPrecachePlugin(config, precacheConfig)
 }
